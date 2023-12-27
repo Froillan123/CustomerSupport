3a9c30b8888637481be717d3c22bee612fb938c3
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2023-12-27 20:28:33 +0800

    STATUS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-let normalCitizenCount = 0;
-let seniorCitizenCount = 0;
-let womenWithChildCount = 0;
-let pwdCount = 0;
 document.addEventListener('DOMContentLoaded', function () 
 {
     let menuIcon = document.querySelector('#menu-icon');
@@ -22,50 +18,57 @@ document.addEventListener('DOMContentLoaded', function ()
     let counter2Queue = [];
     let counter3Queue = [];
 
-    // Add more arrays for additional counters if needed
 
-    // Update the waiting customer information
+
+    function isEmpty(queue) {
+        return queue.length === 0;
+    }
+    
+    function isFull(queue, maxSize) {
+        return queue.length === maxSize;
+    }
+
+
     function updateWaitingCustomers(counterId) {
         let currentCustomerDiv = document.getElementById(`currentCustomer${counterId}`);
         let waitingCustomerDiv = document.getElementById(`waitingCustomer${counterId}`);
         let queue = getCounterQueue(counterId);
 
-        // Display current customer
         if (queue.length > 0) {
             let currentCustomer = queue[0];
             currentCustomerDiv.innerHTML = `<p>Priority: ${currentCustomer.priority} <br> Name: ${currentCustomer.name}</p>`;
         } else {
-            currentCustomerDiv.innerHTML = ''; // No current customer
+            currentCustomerDiv.innerHTML = ''; 
         }
 
-        // Display waiting customer
+
         if (queue.length > 1) {
             let waitingCustomer = queue[1];
-            waitingCustomerDiv.innerHTML = `<p>Priority: ${waitingCustomer.priority} <br> Name: ${waitingCustomer.name}</p><br>`; // Add <br> for line break
+            waitingCustomerDiv.innerHTML = `<p>Priority: ${waitingCustomer.priority} <br> Name: ${waitingCustomer.name}</p><br>`; 
         } else {
-            waitingCustomerDiv.innerHTML = ''; // No waiting customer
+            waitingCustomerDiv.innerHTML = ''; 
         }
 
-        // Check if the queue is updated and if it's a dequeue operation
+    
         if (queue.length < 2 && waitingCustomerDiv.innerHTML !== '') {
-            // This means a dequeue operation occurred, update the view accordingly
+    
             handleQueueUpdate(counterId);
         }
     }
 
-    // Call this function whenever the queue is updated
+    
     function handleQueueUpdate(counterId) {
         updateCustomerBoxes(counterId);
         updateWaitingCustomers(counterId);
     }
 
     function addToCounter(counterId) {
-        // Get form input values
+        
         let customerName = document.getElementById('name').value;
         let customerAge = parseInt(document.getElementById('age').value);
         let customerMessage = document.getElementById('message').value;
 
-        // Check if the form is filled
+       
         if (!isFormFilled(customerName, customerAge, customerMessage)) {
             Swal.fire({
                 icon: 'warning',
@@ -74,8 +77,7 @@ document.addEventListener('DOMContentLoaded', function ()
             });
             return;
         }
-
-        // Age validation
+  
         if (customerAge <= 0 || customerAge > 150) {
             Swal.fire({
                 icon: 'warning',
@@ -85,9 +87,7 @@ document.addEventListener('DOMContentLoaded', function ()
             return;
         }
 
-
-        // Check if the counter is full
-        if (getCounterQueue(counterId).length >= 5) {
+        if (isFull(getCounterQueue(counterId), 5)) {
             Swal.fire({
                 icon: 'warning',
                 title: 'Counter Full',
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function ()
             return;
         }
 
-        // Check for duplicate names across all counters
+    
         if (isNameDuplicate(customerName)) {
             Swal.fire({
                 icon: 'warning',
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', function ()
             return;
         }
 
-        // Age-based validation for Senior Citizen
+       
         let seniorCitizenRadio = document.getElementById('senior-citizen');
         if (customerAge < 60 && seniorCitizenRadio.checked) {
             Swal.fire({
@@ -117,7 +117,6 @@ document.addEventListener('DOMContentLoaded', function ()
             return;
         }
 
-        // Get selected priority
         let priority = document.querySelector('input[name="priority"]:checked');
 
         if (!priority) {
@@ -129,7 +128,6 @@ document.addEventListener('DOMContentLoaded', function ()
             return;
         }
 
-        // Create a new customer object
         let newCustomer = {
             priority: parseInt(priority.value),
             name: customerName,
@@ -137,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function ()
             message: customerMessage,
         };
 
-        // Add type of disease for PWD
+        
         if (priority.value === '4') {
             let disease = document.getElementById('disease').value;
             if (!disease.trim()) {
@@ -151,29 +149,26 @@ document.addEventListener('DOMContentLoaded', function ()
             newCustomer.disease = disease;
         }
 
-        // Insert the new customer into the queue
+       
         let queue = getCounterQueue(counterId);
         queue.push(newCustomer);
 
-        // Sort the queue in descending order based on priority
+        
         queue.sort((a, b) => b.priority - a.priority);
 
-        // Update the customer boxes and waiting customers
+       
         handleQueueUpdate(counterId);
 
         updateChartCounts();
 
-        // Reset the form
         document.getElementById('name').value = '';
         document.getElementById('age').value = '';
         document.getElementById('message').value = '';
 
-        // Reset radio buttons
         document.querySelectorAll('input[name="priority"]').forEach((radio) => {
             radio.checked = false;
         });
 
-        // Reset disease textbox for PWD
         document.getElementById('disease').value = '';
         document.getElementById('diseaseTextbox').style.display = 'none';
     }
@@ -182,10 +177,9 @@ document.addEventListener('DOMContentLoaded', function ()
         let queue = getCounterQueue(counterId);
         let counterBoxes = document.getElementById(`counter${counterId}Boxes`);
 
-        // Clear existing boxes
+    
         counterBoxes.innerHTML = '';
 
-        // Create new boxes for each customer
         queue.forEach((customer) => {
             let newBox = document.createElement('div');
             newBox.className = 'box';
@@ -218,14 +212,20 @@ document.addEventListener('DOMContentLoaded', function ()
                 return counter2Queue;
             case 3:
                 return counter3Queue;
-            // Add more cases for additional counters if needed
+
             default:
                 return [];
         }
     }
 
     function dequeueCustomer(queue, counterId) {
-        if (queue.length > 0) {
+        if (isEmpty(queue)) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'No Customers',
+                text: 'There are no customers in the queue.',
+            });
+        } else {
             let servedCustomer = queue.shift();
             Swal.fire({
                 icon: 'info',
@@ -233,12 +233,6 @@ document.addEventListener('DOMContentLoaded', function ()
                 html: `<p style="font-size: 1.5rem;">Priority: ${servedCustomer.priority},<br> Name: ${servedCustomer.name},<br> Age: ${servedCustomer.age},<br> Message: ${servedCustomer.message}</p>`,
             });
             handleQueueUpdate(counterId);
-        } else {
-            Swal.fire({
-                icon: 'warning',
-                title: 'No Customers',
-                text: 'There are no customers in the queue.',
-            });
         }
     }
 
@@ -271,7 +265,6 @@ document.addEventListener('DOMContentLoaded', function ()
         viewcustomerSection.classList.toggle('active');
     });
 
-    // Close menu when close button is clicked
     document.getElementById('close-menu').addEventListener('click', function () {
         var viewcustomerSection = document.getElementById('viewcustomer');
         viewcustomerSection.classList.remove('active');
@@ -291,15 +284,15 @@ document.addEventListener('DOMContentLoaded', function ()
         let tableBody = document.getElementById('customers-table-body');
         tableBody.innerHTML = ''; // Clear existing rows
 
-        // Iterate over all customers in all counters and add rows to the table
+        
         for (let counterId = 1; counterId <= 3; counterId++) {
             let queue = getCounterQueue(counterId);
 
-            // Sort the queue in descending order based on priority
+            
             queue.sort((a, b) => b.priority - a.priority);
 
             queue.forEach((customer, index) => {
-                let priorityText = getPriorityText(customer.priority); // Get the priority text
+                let priorityText = getPriorityText(customer.priority); 
 
                 let row = document.createElement('tr');
                 row.innerHTML = `
@@ -314,7 +307,8 @@ document.addEventListener('DOMContentLoaded', function ()
         }
     }
 
-    // Helper function to get priority text based on priority value
+    
+
     function getPriorityText(priority) {
         switch (priority) {
             case 1:
@@ -330,18 +324,24 @@ document.addEventListener('DOMContentLoaded', function ()
         }
     }
 
+
+    let normalCitizenCount = 0;
+    let seniorCitizenCount = 0;
+    let womenWithChildCount = 0;
+    let pwdCount = 0;
+
     function updateChartCounts() {
-        // Initialize arrays to store counts for each priority group
+       
         normalCitizenCount = 0;
         seniorCitizenCount = 0;
         womenWithChildCount = 0;
         pwdCount = 0;
     
-        // Iterate over all customers in all counters and update counts
+       
         for (let counterId = 1; counterId <= 3; counterId++) {
             let queue = getCounterQueue(counterId);
     
-            // Update counts based on priority
+           
             queue.forEach((customer) => {
                 switch (customer.priority) {
                     case 1:
@@ -356,21 +356,20 @@ document.addEventListener('DOMContentLoaded', function ()
                     case 4:
                         pwdCount++;
                         break;
-                    // Add more cases for additional priorities if needed
                 }
             });
         }
     
-        // Update the chart with the new counts
+       
         updateChart(normalCitizenCount, seniorCitizenCount, womenWithChildCount, pwdCount);
     }
 
-    // Call updateCustomerTable whenever the customer queue is updated
+  
     function handleQueueUpdate(counterId) {
         updateCustomerBoxes(counterId);
         updateWaitingCustomers(counterId);
-        updateCustomerTable(); // Add this line to update the customer table
-        updateChart(); // Add this line to update the chart
+        updateCustomerTable(); 
+        updateChart();
     }
 
     document.querySelectorAll('input[name="priority"]').forEach((radio) => {
@@ -378,10 +377,10 @@ document.addEventListener('DOMContentLoaded', function ()
             const diseaseTextbox = document.getElementById('diseaseTextbox');
     
             if (radio.value === '4') {
-                // Display the disease textbox when "PWD" is selected
+                
                 diseaseTextbox.style.display = 'block';
             } else {
-                // Hide the disease textbox when another option is selected
+                
                 diseaseTextbox.style.display = 'none';
             }
         });
@@ -389,10 +388,10 @@ document.addEventListener('DOMContentLoaded', function ()
 
     const labels = ['Normal Citizen', 'Senior Citizen', 'Women With Child', 'PWD'];
 
-    // Get the canvas element
+   
     const ctx = document.getElementById('incomeStatementChart').getContext('2d');
 
-    // Create the bar graph
+    
     const chart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -421,8 +420,8 @@ document.addEventListener('DOMContentLoaded', function ()
                 },
                 {
                     label: 'Pwd',
-                    backgroundColor: 'rgba(128, 0, 128, 0.5)', // Purple color
-                    borderColor: 'rgba(128, 0, 128, 1)', // Purple color
+                    backgroundColor: 'rgba(128, 0, 128, 0.5)', 
+                    borderColor: 'rgba(128, 0, 128, 1)', 
                     borderWidth: 1,
                     data: pwdCount,
                 },
@@ -469,8 +468,6 @@ document.addEventListener('DOMContentLoaded', function ()
         },
     });
     
-    // Function to update the chart
-  // Function to update the chart
 function updateChart() {
     chart.data.datasets.forEach((dataset, index) => {
         switch (index) {
@@ -493,7 +490,5 @@ function updateChart() {
     chart.update();
 }
 
-    
-    // Initial chart creation
-    updateChart();
-    });
+updateChart();
+});
